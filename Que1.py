047f19e390a4dcc6e295d0a523be1c77d69b9bf8
Sanskrati Shukla <193257292+sanskratishuklaa@users.noreply.github.com>
2026-04-15 15:14:38 +0530

    Add files via upload

---
+++ Que1.py
@@ -0,0 +1,10 @@
+#What will be the final value of counter after the program finishes execution:
+counter=0
+
+for i in range(5):
+    counter+=1
+
+for i in range(3):
+    counter+=1
+    
+print(counter)
