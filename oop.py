714dbab2dee3c71d4bff85451c5b9f9104c77f2a
Sanskrati Shukla <193257292+sanskratishuklaa@users.noreply.github.com>
2026-04-15 20:32:47 +0530

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,16 @@
+class Car:
+    def __init__(self,color,model,year):
+        self.color=color
+        self.model=model
+        self.year=year
+    def start(self):
+        return f"{self.model} is starting."
+
+    def stop(self):
+        return f"{self.model} is stopping."
+
+myCar = Car( "Red", "Toyota",2019)
+
+print(myCar.color)
+print(myCar.model)
+print(myCar.start())
